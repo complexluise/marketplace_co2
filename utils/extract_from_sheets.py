6ab4e2b42f2fd168be85232911c9eb545bc93800
complexluise <lhiguerac@unal.edu.co>
2023-11-17 21:36:51 -0500

    add docs

--- utils/extract_from_sheets.py
+++ utils/extract_from_sheets.py
@@ -10,14 +10,41 @@ conn = connection("gsheets", type=GSheetsConnection)
 
 
 def select_columns(df: DataFrame, column_start: str, column_end: str) -> DataFrame:
+    """
+    Selects a range of columns from the DataFrame based on column names.
+
+    Args:
+        df (DataFrame): The DataFrame from which to select columns.
+        column_start (str): The name of the starting column.
+        column_end (str): The name of the ending column.
+
+    Returns:
+        DataFrame: A new DataFrame containing columns from column_start to column_end.
+    """
     return df.loc[:, column_start:column_end]
 
 
 def filter_rows_with_data(df: DataFrame, columnas_requeridas: List[str]) -> DataFrame:
+    """
+    Filters rows in the DataFrame that have non-NA values for all specified columns.
+
+    Args:
+        df (DataFrame): The DataFrame to filter.
+        columnas_requeridas (List[str]): List of column names to check for non-NA values.
+
+    Returns:
+        DataFrame: A DataFrame containing rows where all specified columns have non-NA values.
+    """
     return df[df[columnas_requeridas].notna().all(axis=1)]
 
 
 def get_projects() -> DataFrame:
+    """
+    Retrieves project data from a Google Sheets worksheet named 'Proyectos'.
+
+    Returns:
+        DataFrame: A DataFrame containing selected columns and rows with data for projects.
+    """
     df_projects = conn.read(
         worksheet="Proyectos",
         ttl=0,
@@ -38,7 +65,13 @@ def get_projects() -> DataFrame:
     )
 
 
-def get_bonos_project():
+def get_bonos_project() -> DataFrame:
+    """
+    Retrieves data from a Google Sheets worksheet named 'Bonos_Proyecto'.
+
+    Returns:
+        DataFrame: A DataFrame containing selected columns and rows with data for bonos projects.
+    """
     df_bonos_project = conn.read(
         worksheet="Bonos_Proyecto",
         ttl=0,
@@ -54,7 +87,13 @@ def get_bonos_project():
     )
 
 
-def get_bonos_purchased():
+def get_bonos_purchased() -> DataFrame:
+    """
+    Retrieves data from a Google Sheets worksheet named 'Ordenes_Bonos'.
+
+    Returns:
+        DataFrame: A DataFrame containing selected columns and rows with data for purchased bonos.
+    """
     df_bonos_purchased = conn.read(
         worksheet="Ordenes_Bonos",
         ttl=0,
@@ -75,12 +114,17 @@ def get_bonos_purchased():
     )
 
 
-def get_bonos_project_enriched():
+def get_bonos_project_enriched() -> DataFrame:
+    """
+    Enriches bonos project data by merging it with the projects data.
+
+    Returns:
+        DataFrame: A merged DataFrame containing enriched data for bonos projects.
+    """
     df_projects = get_projects()
     df_bonos_project = get_bonos_project()
 
     return merge(df_bonos_project, df_projects, how="inner")
 
-
-# %%
-df = get_bonos_project_enriched
+if __name__ == '__main__':
+    df = get_bonos_project_enriched()
