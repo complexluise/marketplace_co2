bf4affb005f454510baaed72295668f8a328db20
complexluise <lhiguerac@unal.edu.co>
2023-11-19 16:18:15 -0500

    move page

---
+++ pages/available_bonds.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Available Bonds",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        "Get Help": "https://www.extremelycoolapp.com/help",
+        "Report a bug": "https://www.extremelycoolapp.com/bug",
+        "About": "# This is a header. This is an *extremely* cool app!",
+    },
+)
+
+st.write("Bonos disponibles")
+
+
+def main():
+    pass
