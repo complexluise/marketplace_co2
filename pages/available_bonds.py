9028afd7e0d0b66bace4e26068ffb544b856d429
complexluise <lhiguerac@unal.edu.co>
2023-11-19 18:15:27 -0500

    collapse side bar

--- pages/available_bonds.py
+++ pages/available_bonds.py
@@ -4,7 +4,7 @@ st.set_page_config(
     page_title="Available Bonds",
     page_icon="🧊",
     layout="wide",
-    initial_sidebar_state="expanded",
+    initial_sidebar_state="collapsed",
     menu_items={
         "Get Help": "https://www.extremelycoolapp.com/help",
         "Report a bug": "https://www.extremelycoolapp.com/bug",
