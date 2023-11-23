52afd33a1d72bbcd848302acb93c0725c76bde99
complexluise <lhiguerac@unal.edu.co>
2023-11-23 01:43:49 -0500

    some changes

--- pages/available_bonds.py
+++ pages/available_bonds.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import plotly.express as px
+from utils.components import format_as_title
 from utils.extract_from_sheets import get_industry_data
 from utils.models import Proyects, CO2CreditsByProject
 
 st.set_page_config(
     page_title="Available Bonds",
     page_icon="🧊",
-    layout="wide",
+    layout="centered",
     initial_sidebar_state="collapsed",
     menu_items={
         "Get Help": "https://www.extremelycoolapp.com/help",
@@ -15,9 +16,8 @@ st.set_page_config(
     },
 )
 
-st.write("Bonos disponibles")
-
 with st.spinner("Please wait"):
+    format_as_title("Available CO2 Credits")
     df = get_industry_data()
     fig = px.pie(
         df,
